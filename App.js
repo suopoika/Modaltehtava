edf586063fa82c2aea7bb57032fc0a0dd2bfc9f3
suopoika <henrik.junttila@gmail.com>
2024-01-16 16:33:26 +0200

    modal tehtava

--- App.js
+++ App.js
@@ -1,14 +1,43 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Modal, TouchableOpacity } 
+from 'react-native';
+
 
 export default function App() {
+  const [ModalVisible, setModalVisible] = useState(false);
+  const openModal = () => {
+    setModalVisible(true);
+  };
+  const closeModal = () => {
+    setModalVisible(false);
+  };
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TouchableOpacity onPress={openModal}>
+        <Text>Show Modal</Text>
+      </TouchableOpacity>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={ModalVisible}
+        onRequestClose={closeModal}
+      >
+        <View style={styles.modalContainer}>
+          <View style={styles.modalContent}>
+            <Text>This is Modal...</Text>
+            <TouchableOpacity onPress={closeModal}>
+              <Text>Close Modal</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
     </View>
+
+      
+      
+
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,5 +45,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  modalContainer: {
+    flex: 1,
+    backgroundColor: 'black',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalContent: {
+    width: '80%',
+    backgroundColor: '#fff',
+    padding: 40,
+    borderRadius: 10,
   },
 });
